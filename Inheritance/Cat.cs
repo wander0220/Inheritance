0a128f20d079832a85abcc430a59192e7cb4ac81
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-10 13:49:22 +0900

    강아지와 고양이 클래스 추상화

---
+++ Inheritance/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Cat
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+
+        public Cat() { this.Age = 0; }
+        public void Eat() { Console.WriteLine("더 맛있는거 가져와라 닝겐"); }
+        public void Sleep() { Console.WriteLine("하암졸령"); }
+        public void Meow() { Console.WriteLine("고양이 소리내봐 시러 니나내세요"); }
+
+    }
+}
---
+++ Inheritance/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Dog
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+
+        public Dog() { this.Age = 0; }
+        public void Eat() { Console.WriteLine("념념념쫍쫍"); }
+        public void Sleep() { Console.WriteLine("쿨쿨쓰"); }
+        public void Bart() { Console.WriteLine("왈왈왕ㅇ왕ㅇ오아ㅗㅇ알"); }
+    }
+}
